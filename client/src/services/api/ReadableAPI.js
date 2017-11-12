94eca054dfa3d94d9cb95a76c72c0ecfa4fea291
Carlos Costa <jr.carlosad@gmail.com>
2017-11-12 01:54:58 -0200

    feat: ReadableAPI file

---
+++ client/src/services/api/ReadableAPI.js
@@ -0,0 +1,118 @@
+const api = "http://localhost:3001";
+
+let token;
+
+if (process.env.NODE_ENV === 'test') {
+    token = null;
+} else {
+    token = localStorage.token;
+}
+
+const headers = {
+    Accept: "application/json",
+    Authorization: token,
+    "Content-Type": "application/json"
+};
+
+export const getPosts = () =>
+    fetch(`${api}/posts`, { headers })
+        .then(res => res.json());
+
+export const addPost = post =>
+    fetch(`${api}/posts`, {
+        method: "POST",
+        headers,
+        body: JSON.stringify(post)
+    })
+        .then(res => res.json())
+        .then(data => data);
+
+export const upVotePost = (postID) =>
+    fetch(`${api}/posts/${postID}`, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify({ option: "upVote" })
+    })
+        .then(res => res.json())
+        .catch(error => error);
+
+export const downVotePost = (postID) =>
+    fetch(`${api}/posts/${postID}`, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify({ option: "downVote" })
+    })
+        .then(res => res.json())
+        .catch(error => error);
+
+export const upVoteComment = (commentID) =>
+    fetch(`${api}/comments/${commentID}`, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify({ option: "upVote" })
+    })
+        .then(res => res.json())
+        .catch(error => error);
+
+export const downVoteComment = (commentID) =>
+    fetch(`${api}/comments/${commentID}`, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify({ option: "downVote" })
+    })
+        .then(res => res.json())
+        .catch(error => error);
+
+export const deletePost = postID =>
+    fetch(`${api}/posts/${postID}`, {
+        method: "DELETE",
+        headers
+    })
+        .then(res => res.json())
+        .catch(error => error);
+
+export const getComments = postID =>
+    fetch(`${api}/posts/${postID}/comments`, {
+        method: "GET",
+        headers
+    }).then(res => res.json());
+
+export const addComment = comment =>
+    fetch(`${api}/comments/`, {
+        method: "POST",
+        headers,
+        body: JSON.stringify(comment)
+    })
+        .then(res => res.json())
+        .catch(error => error);
+
+export const deleteComment = commentID =>
+    fetch(`${api}/comments/${commentID}`, {
+        method: "DELETE",
+        headers
+    })
+        .then(res => res.json())
+        .catch(error => error);
+
+export const getCategories = () =>
+    fetch(`${api}/categories`, { headers })
+        .then(res => res.json())
+        .catch(error => error);
+
+export const editPost = (post) =>
+    fetch(`${api}/posts/${post.id}`, {
+        method: "PUT",
+        headers,
+        body: JSON.stringify(post)
+    })
+        .then(res => res.json())
+        .catch(error => error);
+
+export const editComment = (comment) =>
+    fetch(`${api}/comments/${comment.id}`, {
+        method: "PUT",
+        headers,
+        body: JSON.stringify(comment)
+    })
+        .then(res => res.json())
+        .catch(error => error);
