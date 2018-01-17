4ba167ec24d72ca1c72a72c7ab6ed994cd7b6a27
Carlos Costa <jr.carlosad@gmail.com>
2018-01-17 04:59:26 -0200

    refactor: SetPostSelected update

--- client/src/scenes/PostDetails/index.js
+++ client/src/scenes/PostDetails/index.js
@@ -13,9 +13,11 @@ class PostDetails extends PureComponent {
         }
     }
 
-    componentWillReceiveProps({ posts }) {
-        if (Object.keys(posts).length !== Object.keys(this.props.posts).length) {
-            this.setPostSelected(posts)
+    componentWillReceiveProps(nextProps) {
+        const { posts } = this.props;
+
+        if (Object.keys(nextProps.posts).length !== Object.keys(posts).length) {
+            this.setPostSelected(nextProps.posts)
         }
     }
 
@@ -23,10 +25,8 @@ class PostDetails extends PureComponent {
         const { id } = this.props.match.params;
         const { setSelected, selected } = this.props;
 
-        if (Object.keys(selected.post).length === 0) {
-            const post = posts.allPosts.map(post => post).filter(post => id === post.id)
-            setSelected('post', ...post)
-        }
+        const post = posts.allPosts.map(post => post).filter(post => id === post.id)
+        setSelected('post', ...post)
     }
 
     render() {
