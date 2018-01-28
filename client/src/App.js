8ad388d63c5daf41764b9e5d20897dda803c3899
Carlos Costa <jr.carlosad@gmail.com>
2018-01-28 14:24:15 -0200

    refactor: Cleaner App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Main from './Main';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import AddPost from './components/AddPost'
 import './styles/sass/main.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Navbar />
-        <Main>
-          <AddPost />
-        </Main>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Header />
+    <Navbar />
+    <Main>
+      <AddPost />
+    </Main>
+  </div>
+);
 
 export default App;
