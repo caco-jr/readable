8aa8d9387d67b55c3c4a34109ed5cc0867d6b157
Carlos Costa <jr.carlosad@gmail.com>
2018-01-28 14:20:15 -0200

    refactor: Header more clean

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,30 +3,27 @@ import twitter from './images/twitter.svg';
 import logo from './images/logo.svg'
 import { Link } from 'react-router-dom';
 
-const Header = () => {
-    return (
-        <header className="header">
-            <section className="container">
+const Header = () => (
+    <header className="header">
+        <section className="container">
+            <Link to={"/"} className="logo" >
+                <span className="logo--text" > READABLE </span>
+                <img className="logo--image" src={logo} alt="logo" />
+            </Link>
 
-                <Link to={"/"} className="logo" >
-                    <span className="logo--text" > READABLE </span>
-                    <img className="logo--image" src={logo} alt="logo" />
-                </Link>
+            <a
+                className="creator__twitter"
+                href="https://twitter.com/cacojr_"
+                target="_blank"
+                rel="noopener noreferrer">
+                <span className="creator__twitter--text" >
+                    @cacojr_
+                </span>
 
-                <a
-                    className="creator__twitter"
-                    href="https://twitter.com/cacojr_"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    <span className="creator__twitter--text" >
-                        @cacojr_
-                    </span>
-
-                    <img className="creator__twitter--image" src={twitter} alt="twitter" />
-                </a>
-            </section>
-        </header>
-    )
-}
+                <img className="creator__twitter--image" src={twitter} alt="twitter" />
+            </a>
+        </section>
+    </header>
+)
 
 export default Header;
